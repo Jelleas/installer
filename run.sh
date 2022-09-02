8fee071afbfca6ae236af023a7c6b46af2f35428
Jelle van Assema <jelle.van.assema@gmail.com>
2022-09-02 12:18:43 +0200

    no commands lijkt okee te zijn in de nieuwste versie van homebrew

--- run.sh
+++ run.sh
@@ -332,14 +332,14 @@ then
   fi
 
   brew_diagnostics=`brew tap-info homebrew/core 2>&1`
-  if [[ $brew_diagnostics =~ (no commands|Not installed) ]]
+  if [[ $brew_diagnostics =~ (Not installed) ]]
   then
     ohai "Homebrew seems to be misconfigured. Shall we try to repair it?"
     wait_for_user
     rm -rf $(brew --prefix)/Library/Taps/homebrew/homebrew-core
     brew tap homebrew/core
     brew_diagnostics=`brew tap-info homebrew/core 2>&1`
-    if [[ $brew_diagnostics =~ (no commands|Not installed) ]]
+    if [[ $brew_diagnostics =~ (Not installed) ]]
     then
       ohai "Homebrew STILL seems to be misconfigured. Shall we try to repair it by reinstalling?"
       wait_for_user
